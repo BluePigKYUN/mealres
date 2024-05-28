70eb337d1c9c403fcdba3cd8f3e4296097efc5f4
user <user@DESKTOP-U9HFUQE>
2024-05-28 17:04:48 +0900

    yr-connection2

--- mealers/src/main/java/com/mealers/controller/FreeCmntController.java
+++ mealers/src/main/java/com/mealers/controller/FreeCmntController.java
@@ -2,6 +2,7 @@ package com.mealers.controller;
 
 import java.io.IOException;
 
+import com.mealers.annotation.Controller;
 import com.mealers.annotation.RequestMapping;
 import com.mealers.annotation.RequestMethod;
 import com.mealers.servlet.ModelAndView;
@@ -9,8 +10,9 @@ import com.mealers.servlet.ModelAndView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 
-@RequestMapping(value = "/freeCmnt/list", method = RequestMethod.GET)
+ @Controller
 public class FreeCmntController {
+	@RequestMapping(value = "/freeCmnt/list", method = RequestMethod.GET)
 	public ModelAndView list(HttpServletRequest req, HttpServletRequest resp) throws ServletException, IOException {
 		ModelAndView mav = new ModelAndView("freeCmnt/list");
 		
